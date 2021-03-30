2d3737778c0ae1c93cc53f4dc76ecab74e694bc3
Eric Creager <eric.creager@yahoo.com>
2021-03-30 13:56:27 -0500

    creating my app for index

--- React.js
+++ React.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom"
-import Compo from "./component"
+import myApp from "./MyApp"
+
+
 ReactDOM.render(<MyApp/>, document.getElementById("root"))
