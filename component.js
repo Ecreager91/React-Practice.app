1f6e59b8d6ce555497fb83ab667614512aa2d220
Eric Creager <eric.creager@yahoo.com>
2021-03-30 13:53:09 -0500

    added html to compo

--- component.js
+++ component.js
@@ -1,5 +1,12 @@
 import React from "react"
 
+function Compo(){
+    return(
+        <div>
+        <h2>This is my Compo Component!</h2>
+        <p>compobreaker!</p>
+        </div>
+    )
+}
 
-
-export default Component
+export default Compo
