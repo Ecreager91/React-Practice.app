3a0e1f7ea9270ad2db72f14d17cd817905ada6c9
Eric Creager <eric.creager@yahoo.com>
2021-03-31 14:12:39 -0500

    created ClassCompo to practice class based compos

---
+++ ClassCompo.js
@@ -0,0 +1,15 @@
+import React from "react";
+ 
+ class ClassCompo extends React.Component{
+     render(){
+         return(
+             <div>
+
+            <h2>Class Compo!</h2>
+            <p>C-C-C-Compo!</p>
+
+             </div>
+         )
+     }
+ }
+ export default ClassCompo
